e901e8f166290e6bb10205515a1f48151bc2db15
Rw552 <rw552@hotmail.com>
2019-11-08 16:19:09 -0600

    commit on 'master'

---
+++ script.js
@@ -0,0 +1,17 @@
+//create new request
+var request = new XMLHttpRequest();
+
+//open a new connection using GET on the url endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = function(){
+    //begin accessing JSON data here
+    var data = JSON.parse(this.response);
+
+    data.forEach(movie=>{
+        console.log(movie.title);
+    });
+};
+
+//send request
+request.send();
